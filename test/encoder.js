69cbdcea7ee6923a52addfc7dbe6a3c26ebcf850
Adrian Sanchez del Campo <qbanguy7@gmail.com>
2017-07-27 14:42:50 -0700

    compatilble node module structure

--- test/encoder.js
+++ test/encoder.js
@@ -1,8 +1,15 @@
-const LPPEncoder = require('../lib/encoder');
+const LPPEncoder = require('../lib').LPPEncoder;
 const assert = require('assert');
 
 describe('encoder', () => {
     it('should return a valid temperature payload', () => {
+        const cyn = new LPPEncoder();
+        cyn.addTemperature(99, 33.0);
+        const pl = cyn.getPayload().toString('base64');
+        assert.equal('Y2cBSg==', pl);
+    });
+
+    it('should return a valid payload with multiple channels', () => {
         const cyn = new LPPEncoder();
         cyn.addAnalogInput(13, -13.131313);
         cyn.addTemperature(99, 33.2);
